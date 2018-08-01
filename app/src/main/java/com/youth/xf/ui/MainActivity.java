4ae95634489389aa7cbaa72059ed24d37db33d86
xiecai1234 <825302814@qq.com>
2018-08-01 22:36:29 +0800

    Initial commit

---
+++ app/src/main/java/com/youth/xf/ui/MainActivity.java
@@ -0,0 +1,65 @@
+package com.youth.xf.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+import com.youth.xf.BaseActivity;
+import com.youth.xf.R;
+import com.youth.xf.ui.adapter.MyFragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends BaseActivity  {
+    private List<Fragment> fragments;
+    private String[] titles = new String[]{"Demo", "API"};
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
+    @Override
+    public void initData(Bundle savedInstanceState) {
+        fragments=new ArrayList<>();
+        fragments.add(new DemoFragment());
+        fragments.add(new APIFragment());
+
+    }
+
+    @Override
+    public void initView() {
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout.addTab(tabLayout.newTab().setText(titles[0]));
+        tabLayout.addTab(tabLayout.newTab().setText(titles[1]));
+        viewPager.setAdapter(new MyFragmentPagerAdapter(getSupportFragmentManager(),fragments,titles));
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                startActivity(new Intent(this,AboutActivity.class)
+                .putExtra("title","关于XFrame"));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
